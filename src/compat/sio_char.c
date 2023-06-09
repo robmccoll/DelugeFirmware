c0e88219935dd001924f9cd26d9dd5036d8d5d20
Rob McColl <rob@robmccoll.com>
2023-06-09 19:31:15 -0400

    x86: skip mirror offset

--- src/compat/sio_char.c
+++ src/compat/sio_char.c
@@ -23,17 +23,17 @@ void uartSetBaudRate(uint8_t scifID, uint32_t baudRate) {
 }
 
 void bufferPICUart(char_t charToSend) {
-  picTxBuffer[uartItems[UART_ITEM_PIC].txBufferWritePos + UNCACHED_MIRROR_OFFSET] = charToSend;
-  uartItems[UART_ITEM_PIC].txBufferWritePos = (uartItems[UART_ITEM_PIC].txBufferWritePos + 1) & (PIC_TX_BUFFER_SIZE - 1);
   /* TODO COMPAT */
   LOG_COMPAT_TODO();
+  picTxBuffer[uartItems[UART_ITEM_PIC].txBufferWritePos] = charToSend;
+  uartItems[UART_ITEM_PIC].txBufferWritePos = (uartItems[UART_ITEM_PIC].txBufferWritePos + 1) & (PIC_TX_BUFFER_SIZE - 1);
   return;
 }
 
 void bufferMIDIUart(char_t charToSend) {
-  midiTxBuffer[uartItems[UART_ITEM_MIDI].txBufferWritePos + UNCACHED_MIRROR_OFFSET] = charToSend;
-  uartItems[UART_ITEM_MIDI].txBufferWritePos = (uartItems[UART_ITEM_MIDI].txBufferWritePos + 1) & (MIDI_TX_BUFFER_SIZE - 1);
   /* TODO COMPAT */
   LOG_COMPAT_TODO();
+  midiTxBuffer[uartItems[UART_ITEM_MIDI].txBufferWritePos] = charToSend;
+  uartItems[UART_ITEM_MIDI].txBufferWritePos = (uartItems[UART_ITEM_MIDI].txBufferWritePos + 1) & (MIDI_TX_BUFFER_SIZE - 1);
   return;
 }
